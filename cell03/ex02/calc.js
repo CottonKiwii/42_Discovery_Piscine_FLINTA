1d8e2cae9a23021d2c9bbc9be6c9e84691925b0b
Julia Wolfram <julia.wolfram@gmx.at>
2025-11-20 21:17:48 +0100

    feat: ex02, calc is calculator for short, I am just using slang chat

--- cell03/ex02/calc.js
+++ cell03/ex02/calc.js
@@ -1,11 +1,47 @@
-let nbr1 = document.getElementById("nbr1");
-let nbr2 = document.getElementById("nbr2");
-let op = document.getElementById("operator").value;
+setInterval(() => {
+	alert("Please, use me...");
+}, 30000);
 
 
 document.getElementById("calculate").addEventListener("click", () => {
-	if (op === "+") {
-		alert(nbr1 + nbr2);
+	let nbr1 = parseInt(document.getElementById("nbr1").value);
+	let nbr2 = parseInt(document.getElementById("nbr2").value);
+	let op = document.getElementById("operator").value;
+
+	if (isNaN(nbr1) || isNaN(nbr2) || nbr1 < 0 || nbr2 < 0) {
+		alert("Error :(");
+		return ;
 	}
-})
+	switch (document.getElementById("operator").value) {
+		case "+":
+			alert(nbr1 + nbr2);
+			console.log(nbr1 + nbr2);
+			break ;
+		case "-":
+			alert(nbr1 - nbr2);
+			console.log(nbr1 - nbr2);
+			break ;
+		case "*":
+			alert(nbr1 * nbr2);
+			console.log(nbr1 * nbr2);
+			break ;
+		case "/":
+			if (nbr2 === 0) {
+				alert("It's over 9000!")
+				console.log("It's over 9000!");
+				break ;
+			}
+			alert(nbr1 / nbr2);
+			console.log(nbr1 / nbr2);
+			break ;
+		case "%":
+			if (nbr2 === 0) {
+				alert("It's over 9000!")
+				console.log("It's over 9000!");
+				break ;
+			}
+			alert(nbr1 % nbr2);
+			console.log(nbr1 % nbr2);
+	}
+});
 
